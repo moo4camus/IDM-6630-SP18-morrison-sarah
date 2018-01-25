2007a0ca94e66fc5031026852a4f5c890e861226
Sararh Morrison <sjean.morrison@gmail.com>
2018-01-25 16:43:36 -0600

    Set Up

---
+++ Week2/code/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+ 	<link rel="stylesheet" href="css/style.css">
+ 	<style>
+@import url('https://fonts.googleapis.com/css?family=Just+Another+Hand|Raleway');
+</style>
+    <title>Cher Horowitz</title>
+  </head>
+  <body>
+    <?php echo "<h1>Cher Horowitz</h1>"; ?>
+ 	<?php include "inc/nav.php" ?>
+
+    <img src="img/featherhat.jpg" alt="Cher Horowitz">
+  </body>
+</html>
